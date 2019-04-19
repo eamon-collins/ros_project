ec5c51c0d97facdcbadd2f9c8ea8b9250ae35793
ec3bd <ec3bd@virginia.edu>
2019-04-19 17:14:03 -0400

    detect circles works

--- catkin_ws/src/fetch/src/fetcher.py
+++ catkin_ws/src/fetch/src/fetcher.py
@@ -14,6 +14,7 @@ from math import pow, atan2, sqrt, tan
 import os
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+import numpy as np
 
 
 
@@ -43,8 +44,30 @@ def handle_image(msg):
 	if cols > 60 and rows > 60:
 		cv2.circle(cv_image, (50,50),10,255)
 
+	gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+	balls = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1, minDist=100, param1=100, param2=30, minRadius=5, maxRadius=250)
+	#print(balls)
+
+	if balls is not None:
+		# convert the (x, y) coordinates and radius of the circles to integers
+		balls = np.round(balls[0, :]).astype("int")
+	 
+		# loop over the (x, y) coordinates and radius of the circles
+		for (x, y, r) in balls:
+			# draw the circle in the output image, then draw a rectangle
+			# corresponding to the center of the circle
+			cv2.circle(cv_image, (x, y), r, (0, 255, 0), 4)
+			#cv2.rectangle(cv_image, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+	 
+		# show the output image
+		#cv2.imshow("output", np.hstack([image, output]))
+
+	#find function in opencv that tells distance and size conversion from pixels to actual size
+
 	cv2.imshow("view from turtlebot3", cv_image)
 	cv2.waitKey(3)
+
+	rospy.sleep(.1)
 
 
 if __name__ == '__main__':
@@ -58,7 +81,10 @@ if __name__ == '__main__':
 	scan = rospy.Subscriber('/scan', LaserScan, handle_scan)
 	camera = rospy.Subscriber('/camera/rgb/image_raw', Image, handle_image)
 	
+	move = Twist()
+	move.linear.x = 1
 
+	turtle_vel.publish(move)
 
 	rate = rospy.Rate(20)
 	while not rospy.is_shutdown():
